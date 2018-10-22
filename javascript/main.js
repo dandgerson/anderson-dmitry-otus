f71323243e8222dea890d39fe06cd38238e9ac91
dandgerson <dandgerson@gmail.com>
2018-10-22 18:52:21 +0300

    setup linter

---
+++ javascript/main.js
@@ -0,0 +1,27 @@
+'use strict';
+
+function sum(a) {
+  const args = [];
+
+  if (typeof a === 'number') {
+    args.push(a);
+  return subSum;
+  }
+  
+  if (typeof a === 'undefined') {
+    return function() {
+      return undefined;
+    }
+  }
+
+  function subSum(b) {
+    if (b) {
+      args.push(b);
+      return subSum;
+    }
+    return args.reduce((a, i) => a + i, 0);
+  }
+
+  return args.reduce((a, i) => a + i, 0);
+}
+console.log(sum(1)(2)(-4)());
