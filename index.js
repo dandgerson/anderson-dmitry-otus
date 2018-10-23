c31bace73b2760510f60f174dcb9085bb553c647
dandgerson <dandgerson@gmail.com>
2018-10-23 12:16:54 +0300

    beautify console output

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 
 const lesson1 = require('./javascript/lesson1');
 
-console.log(lesson1.sum(1)(2)(3)());
-console.log(lesson1.simpleSum(5, 5));
+console.log(`sum(1)(2)(3)() // ${lesson1.sum(1)(2)(3)()}`);
+console.log(`simpleSum(5, 5) // ${lesson1.simpleSum(5, 5)}`);
