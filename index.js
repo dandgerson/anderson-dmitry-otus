af557bf54031bd925bcd49d4ae2623fb922a9b3e
dandgerson <dandgerson@gmail.com>
2018-10-23 15:21:42 +0300

    beatify console.logs

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const lesson1 = require('./javascript/lesson1');
 
-console.log(`sum(1)(2)(3)() // ${lesson1.sum(1)(2)(3)()}`);
 console.log(`simpleSum(5, 5) // ${lesson1.simpleSum(5, 5)}`);
 console.log(`splitSum(5)(5) // ${lesson1.splitSum(5)(5)}`);
+console.log(`sum(undefined)() // ${lesson1.sum(undefined)()}`);
+console.log(`sum(1)(2)(3)() // ${lesson1.sum(1)(2)(3)()}`);
