a654b83d790adee18588d3c0cba509ecf4390b57
Dmitry G. Anderson <dandgerson@gmail.com>
2018-10-31 14:42:37 +0300

    add npm scripts & refactor test.js

--- test.js
+++ test.js
@@ -1,9 +1,4 @@
 const assert = require('assert');
+const expect = require('chai').expect;
 
-describe('Array', () => {
-  describe('#indexOf()', () => {
-    it('should return -1 when the value is not present', () => {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-  });
-});
+const lesson1Test = require('./javascript/lesson1/test');
