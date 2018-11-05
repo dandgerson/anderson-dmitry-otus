b1690c6398f2071f44b55b6e3b3ec9b6f7aa8f4b
Dmitry G. Anderson <dandgerson@gmail.com>
2018-11-05 18:58:05 +0300

    refactor project structure

--- test.js
+++ test.js
@@ -2,4 +2,4 @@ const assert = require('assert');
 const expect = require('chai').expect;
 
 // const lesson1Test = require('./javascript/lesson1/test');
-const converter = require('./converter/test/converter');
+const converter = require('./custom_projects/converter/test/converter');
