caa82203ae087af1d3f4451a424b7ae2c7c6bac8
Dmitry G. Anderson <dandgerson@gmail.com>
2018-11-05 18:45:44 +0300

    init commit

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
 const expect = require('chai').expect;
 
-const lesson1Test = require('./javascript/lesson1/test');
+// const lesson1Test = require('./javascript/lesson1/test');
+const converter = require('./converter/test/converter');
