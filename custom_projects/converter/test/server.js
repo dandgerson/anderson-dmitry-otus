3ff50c2032b005928c90ab3822598ce8d794c966
Dmitry G. Anderson <dandgerson@gmail.com>
2018-11-05 19:19:58 +0300

    add test for rgbToHex

--- custom_projects/converter/test/server.js
+++ custom_projects/converter/test/server.js
@@ -5,15 +5,25 @@ const request = require('request');
 
 describe('Color Code Converter API', function() {
   describe('RGB to Hex conversion', function() {
-    it('returns status 200', function() {
 
+    const url = 'http://localhost:3000/rgbToHex?red=255&green=255&blue=255';
+    
+    it('returns status 200', function() {
+      request(url, function(error, response, body) {
+        expect(response.statusCode).to.equal(200);
+      });
     });
     it('returns the color in hex', function() {
-
+      request(url, function(error, response, body) {
+        expect(body).to.equal('ffffff');
+      });
     });
   });
   
   describe('Hex to RGB conversion', function() {
+
+    const url = 'http://localhost:3000/hexToRgb?hex=00ff00';
+    
     it('returns status 200', function() {
 
     });
