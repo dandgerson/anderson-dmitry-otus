37e991e463b1f8819f17c9743ce855efef73cf34
Dmitry G. Anderson <dandgerson@gmail.com>
2018-11-05 19:40:13 +0300

    add server.js && refactor server.js test

--- custom_projects/converter/test/server.js
+++ custom_projects/converter/test/server.js
@@ -11,27 +11,31 @@ describe('Color Code Converter API', function() {
     it('returns status 200', function() {
       request(url, function(error, response, body) {
         expect(response.statusCode).to.equal(200);
+        done();
       });
     });
     it('returns the color in hex', function() {
       request(url, function(error, response, body) {
         expect(body).to.equal('ffffff');
+        done();
       });
     });
   });
   
   describe('Hex to RGB conversion', function() {
-
+    
     const url = 'http://localhost:3000/hexToRgb?hex=00ff00';
     
     it('returns status 200', function() {
       request(url, function(error, response, body) {
         expect(response.statusCode).to.equal(200);
+        done();
       });
     });
     it('returns the color in RGB', function() {
       request(url, function(error, response, body) {
         expect(body).to.equal('[0, 255, 0]');
+        done();
       });
     });
   });
--- test.js
+++ test.js
@@ -3,3 +3,4 @@ const expect = require('chai').expect;
 
 // const lesson1Test = require('./javascript/lesson1/test');
 const converter = require('./custom_projects/converter/test/converter');
+const server = require('./custom_projects/converter/test/server');
