502c86179511c75354f304a4bf92f8035ed0765c
Dmitry G. Anderson <dandgerson@gmail.com>
2018-11-05 19:12:39 +0300

    add server.js init test

---
+++ custom_projects/converter/test/server.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const expect = require('chai').expect;
+const request = require('request');
+
+describe('Color Code Converter API', function() {
+  describe('RGB to Hex conversion', function() {
+    it('returns status 200', function() {
+
+    });
+    it('returns the color in hex', function() {
+
+    });
+  });
+  
+  describe('Hex to RGB conversion', function() {
+    it('returns status 200', function() {
+
+    });
+    it('returns the color in RGB', function() {
+
+    });
+  });
+});
