e52946066eba7295f10cb4ee485f2a9dd4ea9d3e
Dmitry G. Anderson <dandgerson@gmail.com>
2018-11-10 18:02:04 +0300

    add readme.md

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
 'use strict';
 
-// const lesson1 = require('./javascript/lesson1');
-const lesson2 = require('./javascript/lesson2');
 
-// console.log(`sum() // ${lesson1.sum()}`);
-// console.log(`sum()() // ${lesson1.sum()()}`);
-// console.log(`simpleSum(5, 5) // ${lesson1.simpleSum(5, 5)}`);
-// console.log(`splitSum(5)(5) // ${lesson1.splitSum(5)(5)}`);
-// console.log(`sum(undefined)() // ${lesson1.sum(undefined)()}`);
-// console.log(`sum(1)(2)(3)() // ${lesson1.sum(1)(2)(3)()}`);
-
-const asyncFunctions = [
-  function() {
-    console.log('fn1');
-    return Promise.resolve(1);
-  },
-  function() {
-    return new Promise(resolve => {
-      console.log('fn2');
-      setTimeout(() => resolve(2), 1000);
-    });
-  },
-];
-
-lesson2.promiseReduce(asyncFunctions)
-  .then(console.log)
-  .catch(console.log);
---
+++ node/lesson1/README.md
@@ -0,0 +1,16 @@
+# Node Block
+
+## Lesson 1
+
+### Task Description
+
+#### Write a script ```request``` for server testing
+
+Write a script ```request``` for server testing:
+
+- Create local web server ```server```, which response every ***100ms***
+- Create script ```request``` accepting unput:
+  - number of requests ```n```
+  - type of request ```'parallel'``` or ```'serial'```
+
+The sctipt must sends to local **server** ```n``` *serial* or *parallel* **'http'** requests.
