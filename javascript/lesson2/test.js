413ba887e26e3449fb899061e2aa9411c0a20339
Dmitry G. Anderson <dandgerson@gmail.com>
2018-11-26 16:20:53 +0300

    add deleted folders

---
+++ javascript/lesson2/test.js
@@ -0,0 +1,44 @@
+const expect = require('chai').expect;
+
+const lesson2 = require('./');
+
+const asyncFunctions = [
+  function() {
+    console.log('fn1');
+    return Promise.resolve(1);
+  },
+  function() {
+    return new Promise(resolve => {
+      console.log('fn2');
+      setTimeout(() => resolve(2), 1000);
+    });
+  },
+];
+
+describe('#promiseReduce()', function() {
+
+  context('with invalid arguments', function() {
+    it('shoule throw a TypeError when asyncFunctions invalid', function() {
+      expect(function() {
+        lesson2.promiseReduce();
+      }).to.throw(TypeError);
+    });
+    it('shoule throw a TypeError when reduce invalid', function() {
+      expect(function() {
+        lesson2.promiseReduce(asyncFunctions, 'reducer');
+      }).to.throw(TypeError);
+    });
+    it('shoule throw a TypeError when initialValue invalid', function() {
+      expect(function() {
+        lesson2.promiseReduce(asyncFunctions, (memo,value) => memo * value, 'string');
+      }).to.throw(TypeError);
+    });
+  });
+
+  context('with valid arguments', function() {
+    it('should return Promise', function() {
+      expect(lesson2.promiseReduce(asyncFunctions, (memo,value) => memo * value, 1) instanceof Promise).to.equal(true);
+    });
+  });
+
+});
