15a00cf211d9823f56203b5990292676dc93c6d2
Dmitry G. Anderson <dandgerson@gmail.com>
2018-12-18 11:13:50 +0300

    add test files and folders & refactor variable names & add report function

--- node/lesson-3/tree.js
+++ node/lesson-3/tree.js
@@ -15,15 +15,13 @@ const tree = path => {
 
   return new Promise((resolve, reject) => {
     
-    pathCrawler(path, result, callback);
+    iterator(path, report);
     
-    function callback(count) {
-      if (count.processed >= count.files) {
-        resolve(result);
-      }
+    function callback() {
+      resolve(result);
     }
 
-    function pathCrawler(path, result, callback) {
+    function iterator(path, report) {
       
       fs.readdir(path, (err, files) => {
         count.files += files.length;
@@ -32,18 +30,24 @@ const tree = path => {
           const filePath = `${path}/${file}`;
           
           fs.stat(filePath, (err, stats) => {
-            count.processed++;
-            console.log(count);
             stats.isFile() && result.files.push(filePath);
             if (stats.isDirectory()) {
               result.dirs.push(filePath);
-              pathCrawler(filePath, result, callback);
+              iterator(filePath, report);
             } 
-            callback(count);
+            report(count);
           });
         }
       });
     }
+
+    function report() {
+      count.processed++;
+      if (count.processed === count.files) {
+        callback();
+      }
+    }
+
   });
 };
 
