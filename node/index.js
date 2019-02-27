afcc83255b1c82acd10115c8eb7fcdb2e9530b83
Dmitry G. Anderson <dandgerson@gmail.com>
2019-02-28 01:58:44 +0300

    edit .gitignore & try async/await

--- node/index.js
+++ node/index.js
@@ -1,6 +1,6 @@
-const { promisify } = require('util');
 const fs = require('fs');
 const path = require('path');
+const { promisify } = require('util');
 
 const readdir = promisify(fs.readdir);
 const stat = promisify(fs.stat);
@@ -10,48 +10,59 @@ const stat = promisify(fs.stat);
  * @param {string} directoryPath - path to directoryh
  */
 
-// console.log(path.join(__dirname));
-
 stringifyTree(path.join(__dirname));
 
-async function stringifyTree(directoryPath) {
+async function stringifyTree(root) {
 // implementation
   const result = {
     files: [],
     dirs: []
   };
 
-  let collection = [];
+  const tree = await getNodes(root);
 
-  await collectNodes(await getNodes(directoryPath));
+  await collectNodes(
+    tree, 
+    async node => {
+      const probeResult = await probe(node);
+      probeResult === 'directory' && result.dirs.push(node);
+      probeResult === 'file' && result.files.push(node);
+    }
+  );
 
-  for (let node of collection) {
-    const status = await probe(node);
-    if (status === 'file') result.files.push(node);
-    if (status === 'directory') result.dirs.push(node);
-  }
+  console.log(result);
 
   return result;
   
-  async function collectNodes(nodes) {
-    console.log(nodes);
-    for (let node of nodes) {
-      collection.push(node);
-      const nodePath = `${directoryPath}/${node}`;
-      try {
-        const probeResult = await probe(nodePath);
-        if (probeResult === 'directory') {
-          collectNodes(await getNodes(nodePath));
+  async function collectNodes(nodes, visitor) {
+    for (const node of nodes) {
+      const probeResult = await probe(node);
+      if (probeResult === 'directory') {
+        for (const child of node) {
+          collectNodes(await getNodes(child), visitor);
         }
-
-      } catch(err) {
-        if (err) throw new Error(err);
+      } else {
+        visitor(nodes);
       }
     }
+    
+    // for (let node of nodes) {
+    //   collection.push(node);
+    //   try {
+    //     const probeResult = await probe(root + '\\' + node);
+    //     if (probeResult === 'directory') {
+    //       nodePath = node;
+    //       console.log(`nodePath: ${nodePath}`);
+    //       collectNodes(await getNodes(root + '\\' + nodePath));
+    //     }
+    //   } catch(err) {
+    //     err && console.error(err);
+    //   }
+    // }
   }
 
-  async function getNodes(tree) {
-    return await readdir(tree);
+  async function getNodes(nodePath) {
+    return await readdir(nodePath);
   }
 
   async function probe(path) {
