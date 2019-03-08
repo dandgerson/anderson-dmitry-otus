7e53df7445e1b3ba0fb0bcd16edbd74e57a4f452
Dmitry G. Anderson <dandgerson@gmail.com>
2019-03-08 12:04:47 +0300

    fix conflict

--- index.js
+++ index.js
@@ -3,9 +3,5 @@
 const tree = require('./node/lesson-3/tree');
 // console.log(process.argv);
 tree(process.argv[2])
-<<<<<<< HEAD
   .then(result => console.log(result))
   .catch(err => console.log(err));
-=======
-  .then(result => console.log('result: ', result));
->>>>>>> waterfallOver-pattern
